de96bc4930963819d41555e4fe369d5841129495
Prabu <bsprabuonline@gmail.com>
2023-12-26 12:15:44 +0530

    -- react app setup

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const header = React.createElement('h1', {
     id: 'title',
     test: 'testProps'
-}, 'Hello....');
+}, ' Hello....');
 const root =  ReactDOM.createRoot(document.getElementById('root'));
 
 
 const parent = React.createElement('div', { id : 'parent'}, 
     React.createElement('div' , { id: 'child'}, 
-    [React.createElement('h1', {key:"h1"}, 'Hi I am Nested...H1'), React.createElement('h2', {id: 'h2'}, 'Hi I am Nested...H2')])
+    [React.createElement('h1', {key:"h1"}, 'Hi I am Nested P...H1'), React.createElement('h2', {id: 'h2'}, 'Hi I am Nested...H2')])
 );
 
 
